93283ad419b455e34a875397a47ff78632425531
José Maria Allegue <alleguejosemaria@gmail.com>
2021-12-26 21:59:00 -0300

    Cambios al 26/12/2021

--- Data Analysis - Wes Mckinney/Chapter-7 (Data Wrangling -  Join,  Combine and Reshape)/7-1.py
+++ Data Analysis - Wes Mckinney/Chapter-7 (Data Wrangling -  Join,  Combine and Reshape)/7-1.py
@@ -3,15 +3,30 @@ import pandas as pd
 
 print("Hierarchical Indexing\n")
 
-df = pd.DataFrame(np.random.randn(9),
-                  index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'],
-                         [1, 2, 3, 1, 3, 1, 2, 2, 3]])
+# doble indexacion en
+df = pd.DataFrame({'num_legs': [4, 4, 2, 2],
+                  'num_wings': [0, 0, 2, 2],
+                   'class': ['mammal', 'mammal', 'mammal', 'bird'],
+                   'animal': ['cat', 'dog', 'bat', 'penguin'],
+                   'locomotion': ['walks', 'walks', 'flies', 'walks']})
 
+df = df.set_index(['class', 'animal', 'locomotion'])
 print(df)
 print()
-print(df.index)
+# seleccion de un elemento en el primer indece
+print(df.xs("mammal"))
 print()
-print(df.loc["b"])
+# seleccion de 2 elementos en indices continuos
+print(df.xs(("mammal", "cat")))
 print()
-print(df.loc[:, 2])
+# seleccion de 2 elementos en indeces no continuos
+print(df.xs(("mammal", "walks"), level=["class", "locomotion"]))
 print()
+# seleccion de indece que no es el primero
+print(df.xs("cat", level=1))
+print()
+# seleccion de columnas
+print(df.xs("num_legs", axis=1))
+print()
+#print(df.xs("walks", level=2)["num_legs"])
+print(df[df["num_wings"] > 1].xs("flies", level=2))
