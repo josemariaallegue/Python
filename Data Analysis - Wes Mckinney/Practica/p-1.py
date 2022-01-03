c7405bad2555affda0317b8c05aafb1468082f6e
José Maria Allegue <alleguejosemaria@gmail.com>
2022-01-03 00:12:52 -0300

    Cambios al 03/01/2022

--- Data Analysis - Wes Mckinney/Practica/p-1.py
+++ Data Analysis - Wes Mckinney/Practica/p-1.py
@@ -8,5 +8,3 @@ columnas = ["COLUMNA" + str(x) for x in range(1, 12)]
 
 df = pd.read_csv(Path(ruta, "C240000000002111.txt"), sep="\t",
                  encoding="ANSI", header=0, skiprows=1, names=columnas, skipfooter=1, engine="python")
-
-# prueba git
