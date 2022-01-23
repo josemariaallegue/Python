d850b5ea919acddf2a5a644afbab5367ed382509
José Maria Allegue <alleguejosemaria@gmail.com>
2022-01-22 23:11:59 -0300

    Cambios al 22/01/2022

--- Django - Corey Schafer/course_project/blog/views.py
+++ Django - Corey Schafer/course_project/blog/views.py
@@ -1,20 +1,13 @@
+from .models import Post
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-posts = [
-    {"author": "natalia",
-     "title": "blog post 1",
-     "content": "1 post content",
-     "datePosted": "27 august 2019"},
-    {"author": "natalia2",
-     "title": "blog post 2",
-     "content": "2 post content",
-     "datePosted": "27 august 2021"}]
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
